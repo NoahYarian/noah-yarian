b7844b0f5fed15098b082bb258f9760e50bd1299
Noah Yarian <noah.yarian@gmail.com>
2015-09-07 23:54:45 -0500

    added about me content and worked on styling. Added ascii planet and pointer hover css

--- src/main.js
+++ src/main.js
@@ -16,4 +16,34 @@ $(function() {
   });
 });
 
-console.log("hola!");
+console.log(`
+
+                                                     ___
+    Welcome.                                    ,o88888
+                                               ,o8888888'
+                         ,:o:o:oooo.        ,8O88Pd8888"
+                     ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"
+                   ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"
+                  , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"
+                 , ..:.::o:ooOoOO8O888O8O,COCOO"
+                , . ..:.::o:ooOoOOOO8OOOOCOCO"
+                 . ..:.::o:ooOoOoOO8O8OCCCC"o
+                    . ..:.::o:ooooOoCoCCC"o:o
+                    . ..:.::o:o:,cooooCo"oo:o:
+                 '   . . ..:.:cocoooo"'o:o:::'
+                 .'   . ..::ccccoc"'o:o:o:::'
+                :.:.    ,c:cccc"':.:.:.:.:.'
+              ..:.:"''::::c:"'..:.:.:.:.:.'
+            ...:.'.:.::::"'    . . . . .'
+           .. . ....:."' '   .  . . ''
+         . . . ...."'
+         .. . ."'     -hrr-
+        .
+
+`);
+
+$('.pointer *').hover(function(){
+    $('.pointer *').css("color", "#FF9900");
+    }, function(){
+    $('.pointer *').css("color", "white");
+});
